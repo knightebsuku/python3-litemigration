17994d11026cbe0fb1cca4f6aa31d2ecaa6d37fb
zeref <complexstu@gmail.com>
2015-03-05 17:18:31 +0200

    dev

--- litemigration/database.py
+++ litemigration/database.py
@@ -3,12 +3,15 @@
 import sqlite3
 import datetime as dt
 import logging
+import MySQLdb
+import psycopg2
 
 class Database(object):
     "Create migration control"
-    def __init__(self, db,logger = None):
-        self.connect = sqlite3.connect(db)
-        self.cursor = self.connect.cursor()
+    def __init__(self, db_type,details,logger = None):
+        self.db_type = db_type
+        self.details = details
+        self.cursor = self.check_database_type()
         self.logger = logging.getLogger(__name__)
 
     def initialise(self):
@@ -24,6 +27,18 @@ class Database(object):
         except sqlite3.OperationalError as e:
             self.logger.exeception(e)
             exit()
+
+    def check_database_type(self):
+        database_dict = {'postgresql': self.postgres,
+                         'mysql': self.mysql,
+                         'sqlite': self.sqlite}
+        try:
+            choosen_db = database_dict[self.db_type]
+            self.connect = choosen_db()
+        except KeyError:
+            logging.error("Unknown database")
+            exit()
+            
 
     def add_schema(self, change_list):
         "Add new schema changes"
@@ -42,3 +57,23 @@ class Database(object):
                     self.logger.error("Unable to add schema {}".format(change_id),
                                          exc_info=True)
                     exit()
+
+    def postgresql(self):
+        "create postgresql connections"
+        
+        try:
+            connect = psycopg2.connect(self.details)
+            return connect
+        except psycopg2.Error as e:
+            logging.error("Unable to connect to postgresql")
+            logging.exeception(e)
+            exit()
+    def mysql(self):
+        "create mysql connections"
+        connect = MySQLdb.connect(self.details)
+        return connect
+    def sqlite(self):
+        "create sqlite connections"
+        connect = sqlite3.connect(self.details)
+        return connect
+        
