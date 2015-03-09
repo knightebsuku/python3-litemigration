dbdd235062d8440ecda10b564edc09812d65db57
zeref <complexstu@gmail.com>
2015-03-09 17:07:51 +0200

    add support for postgresql

--- litemigration/database.py
+++ litemigration/database.py
@@ -6,53 +6,83 @@ import logging
 
 class Database(object):
     "Create migration control"
-    def __init__(self, db_type, details, logger = None):
+    def __init__(self, db_type, host=None, port=None, user=None,
+                 password=None, database=None):
         self.db_type = db_type
-        self.details = details
-        self.cursor = self.check_database_type()
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+        self.details = ""
         self.logger = logging.getLogger(__name__)
+        self.connect = self.get_connector()
+        self.cursor = self.connect.cursor()
 
-    def initialise(self):
-        "Create new database on new start"
-        try:
-            self.connect.execute("CREATE TABLE migration("\
-                             'id INTEGER PRIMARY KEY NOT NULL,'\
-                             'version INTEGER UNIQUE NOT NULL,'\
-                             'date TIMESTAMP NOT NULL)')
-            self.cursor.execute("INSERT INTO migration(version,date) VALUES(0,?)", (dt.datetime.now(),))
-            self.connect.commit()
-            self.connect.close()
-        except sqlite3.OperationalError as e:
-            self.logger.exception(e)
-            exit()
-
-    def _check_database_type(self):
-        database_dict = {'postgresql': self._postgresql,
-                         'mysql': self._mysql,
+    def get_connector(self):
+        all_db = {'postgresql': self._postgresql,
                          'sqlite': self._sqlite}
         try:
-            choosen_db = database_dict[self.db_type]
-            self.connect = choosen_db()
+            connect = all_db[self.db_type]()
+            return connect
         except KeyError:
-            logging.error("Unknown database")
+            self.logger.critical("Unknown database or not supported")
+            exit()
+
+    def get_sql(self):
+        sqlite_create = ["CREATE TABLE migration("\
+          'id INTEGER PRIMARY KEY NOT NULL,'\
+          'version INTEGER UNIQUE NOT NULL,'\
+          'date TIMESTAMP NOT NULL)',
+          "INSERT INTO migration(version,date) VALUES(0,?)"]
+          
+        pg_create = ["CREATE TABLE migration("\
+          'id SERIAL PRIMARY KEY,'\
+          'version INTEGER NOT NULL,'\
+          'date DATE NOT NULL)',
+          "INSERT INTO migration(version,date) VALUES(0,%s)"]
+          
+        all_sql = {'postgresql': pg_create,
+                   'sqlite': sqlite_create}
+
+        return all_sql[self.db_type]
+
+    def initialise(self):
+        [create_table, initial_insert] = self.get_sql()
+        try:
+            self.cursor.execute(create_table)
+            self.cursor.execute(initial_insert,
+                                (dt.datetime.now(),))
+            self.connect.commit()
+            print("OK")
+            self.logger.info("Database has been created")
+        except Exception as e:
+            self.logger.error("Unable to add migration table")
+            self.logger.error(e)
             exit()
             
-
     def add_schema(self, change_list):
         "Add new schema changes"
+        if self.db_type == 'postgresql':
+            insert_sql = "INSERT INTO migration(version,date) VALUES(%s,%s)"
+        elif self.db_type == 'sqlite':
+            insert_sql = "INSERT INTO migration(version,date) VALUES(?,?)"
+            
         for (change_id, sql_statement) in change_list:
             self.cursor.execute('SELECT max(version) from migration')
             (max_id,) = self.cursor.fetchone()
             if max_id >= change_id:
                 self.logger.info("schema change {} is smaller the lastest schema change {}"\
-                                 " or new change id has already being applied".format(change_id, max_id))
+                                 " or new change id has already being applied".format(change_id,
+                                                                                      max_id))
             else:
                 try:
-                    self.connect.execute(sql_statement)
-                    self.connect.execute("INSERT INTO migration(version,date) VALUES(?,?)",
+                    self.cursor.execute(sql_statement)
+                    self.cursor.execute(insert_sql,
                                          (change_id, dt.datetime.now(),))
                     self.connect.commit()
-                except sqlite3.OperationalError:
+                    self.logger.info("new schemas added")
+                except Exception:
                     self.logger.error("Unable to add schema {}".format(change_id),
                                          exc_info=True)
                     exit()
@@ -61,7 +91,7 @@ class Database(object):
         "create postgresql connections"
         try:
             import psycopg2
-            connect = psycopg2.connect(self.details)
+            connect = psycopg2.connect(self.database)
             return connect
         except ImportError:
             self.logger.error("Unable to find python postgresql module")
@@ -70,32 +100,13 @@ class Database(object):
             logging.error("Unable to connect to postgresql")
             logging.exception(e)
             exit()
-            
-    def _mysql(self):
-        "create mysql connections"
-        try:
-            import MySQLdb
-            connect = MySQLdb.connect(self.details)
-            return connect
-        except ImportError:
-            self.logger.error("Unable to find python mysql module")
-            exit()
-        except MySQLdb.Error:
-            self.logger.error("Unable to connect to the mysql database")
-            exit()
-            
+
     def _sqlite(self):
         "create sqlite connections"
         import sqlite3
         try:
-            connect = sqlite3.connect(self.details)
-            sql_command = "CREATE TABLE migration("\
-                             'id INTEGER PRIMARY KEY NOT NULL,'\
-                             'version INTEGER UNIQUE NOT NULL,'\
-                             'date TIMESTAMP NOT NULL)'
-            return connect, sql_command
+            connect = sqlite3.connect(self.database)
+            return connect
         except sqlite3.OperationalError:
             self.logger.error("Unable to connect to sqlite3 database")
             exit()
-        
-        
