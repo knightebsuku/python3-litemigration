73ab534096de91fd757ebb18c989ca27e5bdc350
Lunga Mthembu <shellcomplex7.gmail.com>
2015-03-09 06:32:26 +0200

    dev

--- litemigration/database.py
+++ litemigration/database.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
-import sqlite3
 import datetime as dt
 import logging
-import MySQLdb
-import psycopg2
+
 
 class Database(object):
     "Create migration control"
-    def __init__(self, db_type,details,logger = None):
+    def __init__(self, db_type, details, logger = None):
         self.db_type = db_type
         self.details = details
         self.cursor = self.check_database_type()
@@ -25,13 +23,13 @@ class Database(object):
             self.connect.commit()
             self.connect.close()
         except sqlite3.OperationalError as e:
-            self.logger.exeception(e)
+            self.logger.exception(e)
             exit()
 
-    def check_database_type(self):
-        database_dict = {'postgresql': self.postgres,
-                         'mysql': self.mysql,
-                         'sqlite': self.sqlite}
+    def _check_database_type(self):
+        database_dict = {'postgresql': self._postgresql,
+                         'mysql': self._mysql,
+                         'sqlite': self._sqlite}
         try:
             choosen_db = database_dict[self.db_type]
             self.connect = choosen_db()
@@ -46,7 +44,8 @@ class Database(object):
             self.cursor.execute('SELECT max(version) from migration')
             (max_id,) = self.cursor.fetchone()
             if max_id >= change_id:
-                self.logger.info("schema change {} is smaller the lastest schema change {} or new change id has already being applied".format(change_id, max_id))
+                self.logger.info("schema change {} is smaller the lastest schema change {}"\
+                                 " or new change id has already being applied".format(change_id, max_id))
             else:
                 try:
                     self.connect.execute(sql_statement)
@@ -58,22 +57,45 @@ class Database(object):
                                          exc_info=True)
                     exit()
 
-    def postgresql(self):
+    def _postgresql(self):
         "create postgresql connections"
-        
         try:
+            import psycopg2
             connect = psycopg2.connect(self.details)
             return connect
+        except ImportError:
+            self.logger.error("Unable to find python postgresql module")
+            exit()
         except psycopg2.Error as e:
             logging.error("Unable to connect to postgresql")
-            logging.exeception(e)
+            logging.exception(e)
             exit()
-    def mysql(self):
+            
+    def _mysql(self):
         "create mysql connections"
-        connect = MySQLdb.connect(self.details)
-        return connect
-    def sqlite(self):
+        try:
+            import MySQLdb
+            connect = MySQLdb.connect(self.details)
+            return connect
+        except ImportError:
+            self.logger.error("Unable to find python mysql module")
+            exit()
+        except MySQLdb.Error:
+            self.logger.error("Unable to connect to the mysql database")
+            exit()
+            
+    def _sqlite(self):
         "create sqlite connections"
-        connect = sqlite3.connect(self.details)
-        return connect
+        import sqlite3
+        try:
+            connect = sqlite3.connect(self.details)
+            sql_command = "CREATE TABLE migration("\
+                             'id INTEGER PRIMARY KEY NOT NULL,'\
+                             'version INTEGER UNIQUE NOT NULL,'\
+                             'date TIMESTAMP NOT NULL)'
+            return connect, sql_command
+        except sqlite3.OperationalError:
+            self.logger.error("Unable to connect to sqlite3 database")
+            exit()
+        
         
